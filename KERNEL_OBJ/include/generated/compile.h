5b9563767d6aa612978dc224581168635996b56a
Karthik <karthikeyan.psy@gmail.com>
2014-06-23 21:45:18 +0530

    OTG Enable - WIP-1

--- KERNEL_OBJ/include/generated/compile.h
+++ KERNEL_OBJ/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 14 02:50:41 IST 2014"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Jun 18 23:01:23 IST 2014"
 #define LINUX_COMPILE_BY "karthik"
 #define LINUX_COMPILE_HOST "karthik-R470-R420"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
